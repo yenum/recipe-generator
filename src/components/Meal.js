385cdf0a5c9612c76ab2387be6de4f0b3e67f757
Yenum <87123146+yenum@users.noreply.github.com>
2021-10-25 19:56:17 +0100

    add JSON file

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,30 +1,39 @@
 import React, {useState, useEffect} from "react"
 
-const url =  '/data.json'
-const Meal =() => {
-    const [food, setFood] = useState([])
+const data = require('../data.json').recipes
 
-    const fetchFood = async () => {
-        const response = await fetch(url)
-        const data = await response.json()
+//const url =  '../../data.json'
+const Meal =() => {
+    const [food, setFood] = useState(data)
+
+    const fetchFood =  () => {
         console.log(data.recipes)
         setFood(data.recipes)
     }
 
     useEffect(() => {
-        fetchFood()
+    //fetchFood()
 
     }, [])
 
 
     return(
+       <> 
+
+<div className="button">
+        <button onClick={() => fetchFood()} className="btn">
+          Generate Meal
+        </button>
+      </div>
+
         <section>
             {food.map((f) => {
                 const {analyzedInstructions, id, image, title, extendedIngredients,servings} = f
 
                 return(
-                    <article key={id}>
+                    <article key={id} className="recipes">
                         <h3> {title}</h3>
+                        <img src={image} alt={title}/>
                         <h4> {servings} </h4>
                         <p>Ingredients</p>
                         <ul>
@@ -33,18 +42,29 @@ const Meal =() => {
                         })}
                         </ul>
 
-                        <ul>
-                        {analyzedInstructions.map((d,i)=>{
-                            return <li key={i}>{d.steps}</li>
-                        })}
-                        </ul>
-                         
-                        <img src={image} alt={title}/>
+                        <p>Steps</p>
 
+                        {analyzedInstructions.length === 0 ? (
+                         <span>
+                       No Steps Required
+                         </span>
+                             ) : (
+                            <ol>
+                       {analyzedInstructions.map((d, i) => {
+                           return d.steps.map((x, c) => {
+                    return <li key={`ing-${i}-${c}`}>{x.step}</li>;
+                                     });
+                                      })}
+                               </ol>
+                            )}
+                         
+                       
                     </article>
                 )
             })}
         </section>
+        </>
+        
     )
     
 }
